7df47a786cc2957aca67dda2d4d263ff9b9f1598
Dylan Staatz <dylanstaatz@gmail.com>
2021-03-17 23:40:22 -0400

    Implemented downloading and converting audio files from google audiosett

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # weak-audio-cls
 Classifiers for weakly labeled audio data
+
+Install requirements
+```
+pip install -r requirements.txt
+```
+
+Also ffmpeg will need to be install on the system.
---
+++ import_google_audioset.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+
+from __future__ import unicode_literals
+import youtube_dl
+
+import csv
+import json
+
+# Returns list of dict of labels
+def load_csv(filename):
+    data = list()
+    with open(filename) as file:
+        r = csv.reader(file)
+
+        # Skip first 3 lines
+        r.__next__(), r.__next__(), r.__next__()
+
+        # iterate through rest
+        for line in r:
+            d = dict()
+            d["ytid"] = line[0]
+            d["start_seconds"] = float(line[1])
+            d["end_seconds"] = float(line[2])
+            positive_labels = ','.join(line[3:])
+            d["positive_labels"] = positive_labels[2:len(positive_labels)-1].split(',')
+            data.append(d)
+    return data
+
+# Returns list of dict of label information
+def load_ontology():
+    with open("google_audioset_meta/ontology.json") as file:
+        return json.load(file)
+
+# Downloads all the youtube video ids as mp3s saved to the given folder
+def bulk_download(folder, ids):
+
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": folder + "%(id)s.%(ext)s",
+        "ignoreerrors": True,
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
+        }]
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(ids)
+
+# Downloads all ids in meta_data that have the given label_id to the given folder
+def download_labeled(meta_data, label_id, folder):
+    ids = list()
+    for d in meta_data:
+        if label_id in d["positive_labels"]:
+            ids.append(d["ytid"])
+    bulk_download(folder, ids)
+
+# Downloads all ids in meta_data that have a label in the list label_ids to the given folder
+def download_multiple_labels(meta_data, label_ids, folder):
+    ids = list()
+    for d in meta_data:
+        for label_id in label_ids:
+            if label_id in d["positive_labels"]:
+                ids.append(d["ytid"])
+                break
+    bulk_download(folder, ids)
+
+# Test function
+if __name__ == "__main__":
+
+    data1 = load_csv("google_audioset_meta/eval_segments.csv")
+    for i in range(0, 10):
+        print(data1[i])
+    print()
+
+    data2 = load_csv("google_audioset_meta/balanced_train_segments.csv")
+    for i in range(0, 5):
+        print(data2[i])
+    print()
+
+    data3 = load_csv("google_audioset_meta/unbalanced_train_segments.csv")
+    for i in range(0, 5):
+        print(data3[i])
+    print()
+
+    ontology = load_ontology()
+    print(ontology[0])
+    print()
+
+    ids = [x["ytid"] for x in data1]
+    folder = "data/test/"
+    print(ids[30:50])
+    bulk_download(folder, ids[30:50])
+
+    labels = ["/m/04rlf", "/t/dd00004", "/t/dd00005", "/m/03l9g"]
+    # download_labeled(data1[0:10], labels[0], folder)
+    download_multiple_labels(data1[0:10], labels, folder)
+
