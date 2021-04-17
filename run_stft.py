43a9a0037ad67a753020dcd8235e9bf41e69679b
Liam Smith <liam97smith@gmail.com>
2021-04-17 14:27:51 -0400

    Added PCA code.

--- run_stft.py
+++ run_stft.py
@@ -3,10 +3,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.signal import stft
-from scipy.linalg import svd
+# from scipy.linalg import svd
 
 from import_google_audioset import *
 from pydub.utils import mediainfo
+
+from sklearn.decomposition import PCA
 
 # eval_meta = load_csv("google_audioset_meta/eval_segments.csv")
 balanced_meta = load_csv("google_audioset_meta/balanced_train_segments.csv")
@@ -24,38 +26,39 @@ metadata = [audio[i][0] for i in range(len(audio))]
 data = [np.array(audio[i][1].set_channels(1).get_array_of_samples()) for i in range(len(audio))]
 freqs = [int(mediainfo("data/balanced/" + item["ytid"] + ".m4a")['sample_rate']) for item in metadata]
 stfts = [stft(data[i], fs=freqs[i], nperseg=512, nfft=1024) for i in range(len(data))]
-svds = []
-for i in range(len(stfts)):
-    print("Calculating svd {} of shape {}".format(metadata[i]['ytid'], str(stfts[i][2].shape)))
-    svds.append(svd(stfts[i][2]))
-
-print("Sample frequencies:", stfts[0][0].shape)
-print("Segment times:", stfts[0][1].shape)
-print("STFT:", stfts[0][2].shape)
-
-print("U matrix:", svds[0][0].shape)
-print("Sigular values:", svds[0][1].shape)
-print("Vh matrix:", svds[0][2].shape)
+# svds = []
+# for i in range(len(stfts)):
+#     print("Calculating svd {} of shape {}".format(metadata[i]['ytid'], str(stfts[i][2].shape)))
+#     svds.append(svd(stfts[i][2]))
+#
+# print("Sample frequencies:", stfts[0][0].shape)
+# print("Segment times:", stfts[0][1].shape)
+# print("STFT:", stfts[0][2].shape)
+#
+# print("U matrix:", svds[0][0].shape)
+# print("Sigular values:", svds[0][1].shape)
+# print("Vh matrix:", svds[0][2].shape)
 
 # print(stfts[0][0])
 # print()
 # print(stfts[0][2][:,0])
 
-# plot the first stft
-print(np.abs(stfts[0][2]).min())
-print(np.abs(stfts[0][2]).max())
-plt.pcolormesh(stfts[0][1], stfts[0][0], np.abs(stfts[0][2]), vmin=0, vmax=250, shading="auto")
-plt.title("STFT Magnitude of %s" % (metadata[0]["ytid"]))
-plt.ylabel("Frequency [Hz]")
-plt.xlabel("Time [sec]")
-plt.show()
+def plot_stft(i):
+    # plot the  stft
+    # print(np.abs(stfts[i][2]).min())
+    # print(np.abs(stfts[i][2]).max())
+    plt.pcolormesh(stfts[i][1], stfts[i][0], np.abs(stfts[i][2]), vmin=0, vmax=250, shading="auto")
+    plt.title("STFT Magnitude of %s" % (metadata[i]["ytid"]))
+    plt.ylabel("Frequency [Hz]")
+    plt.xlabel("Time [sec]")
+    plt.show()
 
-# plot the second stft
-print(np.abs(stfts[1][2]).min())
-print(np.abs(stfts[1][2]).max())
-plt.pcolormesh(stfts[1][1], stfts[1][0], np.abs(stfts[1][2]), vmin=0, vmax=250, shading="auto")
-plt.title("STFT Magnitude of %s" % (metadata[1]["ytid"]))
-plt.ylabel("Frequency [Hz]")
-plt.xlabel("Time [sec]")
-plt.show()
+
+pca = PCA(n_components=100)
+xvals = [np.abs(stft[2]).T for stft in stfts]
+# X = np.concatenate([np.abs(stft[2]) for stft in stfts], axis=1)
+X = np.concatenate(xvals, axis=0)
+pca.fit(X)
+t_xvals = [pca.transform(x) for x in xvals]
+t_X = pca.transform(X)
 
